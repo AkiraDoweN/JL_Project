73335ec6657ab7693842043be1a37f8bae7272e3
KitsuneDoweN <65587706+KitsuneDoweN@users.noreply.github.com>
2020-05-20 16:55:58 +0900

    Player_Dash

---
+++ Assets/Resources/Script/PlayerScript/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int Damage = 20;
+    private Transform weapon;
+
+    void Start()
+    {
+        weapon = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Collider[] collisions = Physics.OverlapCapsule(weapon.position, weapon.position, 0.3f);
+        foreach (Collider collider in collisions)
+        {
+            if (collider.gameObject.tag == "Monster")
+            {
+                PlayerAttack(collider);
+                break;
+            }
+        }
+    }
+    void PlayerAttack(Collider colider)
+    {
+       // colider.gameObject.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+    }
+
+}
--- Assets/Resources/Script/PlayerScript/Player.cs
+++ Assets/Resources/Script/PlayerScript/Player.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public float MoveSpeed = 20.0f;
+    public int MoveSpeed = 20;
+
+    public int startHp = 100;
+    public int NowHp = 0;
+
     float HorizontalMove;
     float VerticalMove;
 
     private Animator animator;
+    public Slider hpSlider;
 
     Vector3 look;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        NowHp = startHp;
     }
 
     void Update()
@@ -28,14 +35,38 @@ public class Player : MonoBehaviour
             this.transform.rotation = Quaternion.LookRotation(look);
             this.transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
             animator.SetInteger("playerState", 1);
-        }
+            if (Input.GetKey(KeyCode.Space)) // dash
+            {
+                MoveSpeed = 40;
+            }
+            else
+                MoveSpeed = 20;
+        }   
         else if (Input.GetKey(KeyCode.C))
         {
             animator.SetInteger("playerState", 2);
         }
+
         else
         {
             animator.SetInteger("playerState", 0);
+            MoveSpeed = 20;
         }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        animator.SetInteger("playerState", 3);
+        NowHp -= damage;
+        hpSlider.value = NowHp;
+        if(NowHp <= 0)
+        {
+            Dead();
+        }
+    }
+    public void Dead()
+    {
+        //animator.SetInteger("playerState", 4); //죽는 애니메이션
+
     }
 }
