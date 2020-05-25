00b9843e39ae3751bb80df6c0f927b1095c4fd15
To_Ran <41686179+biru100@users.noreply.github.com>
2020-05-26 03:27:02 +0900

    살려 2

--- Assets/Resources/Script/Monster/Gurgugi.cs
+++ Assets/Resources/Script/Monster/Gurgugi.cs
@@ -1,17 +1,135 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Gurgugi : MonoBehaviour
+public class Gurgugi : Monster
 {
     public bool Property = true;
+    [SerializeField]
+    private Material[] skin;
+    NavMeshAgent nav;
+    public Renderer renderer;
+
+    public GameObject attack;
+
+    Animator anim;
+    TYPE type;
+
+    private Transform Target;
+    float[] StateTimechk = new float[3];
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.SetInteger("state", 3);
+        //SetType();
+        nav = GetComponent<NavMeshAgent>();
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    void Update()
+    void SetType()
     {
-        
+        type = (TYPE)Random.Range(0, 3);
+        switch (type)
+        {
+            case TYPE.RAIN:
+                renderer.material = skin[0];
+                break;
+            case TYPE.CLOUD:
+                renderer.material = skin[1];
+                break;
+            case TYPE.WIND:
+                renderer.material = skin[2];
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerWeapon")
+        {
+            Dead();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        SetState();
+    }
+
+    void SetState()
+    {
+        nav.SetDestination(transform.position);
+        switch (anim.GetInteger("state"))
+        {
+            case 0:
+                Attack();
+                break;
+            case 1:
+                Hit();
+                break;
+            case 2:
+                Dead();
+                break;
+            case 3:
+                Move();
+                break;
+        }
+    }
+
+
+
+    void Move()
+    {
+        if ((Target.position - transform.position).magnitude < 7)
+        {
+            anim.SetInteger("state", 0);
+        }
+        else
+        {
+            nav.SetDestination(Target.position);
+        }
+    }
+
+    void Attack()
+    {
+        if(StateTimechk[0] == 0)
+        {
+            StateTimechk[0] = Time.time;
+            nav.speed = 0;
+            attack.SetActive(true);
+        }
+        else if(Time.time - StateTimechk[0] >= 0.7115385f)
+        {
+            nav.speed = 3.5f;
+            anim.SetInteger("state", 3);
+            attack.SetActive(false);
+        }
+        nav.SetDestination(Target.position);
+    }
+    
+    void Hit()
+    {
+        if (StateTimechk[1] == 0)
+        {
+            StateTimechk[1] = Time.time;
+        }
+        else if (Time.time - StateTimechk[1] >= 0.53125f)
+        {
+            anim.SetInteger("state", 3);
+        }
+    }
+
+    void Dead()
+    {
+        anim.SetInteger("state", 2);
+        if (StateTimechk[2] == 0)
+        {
+            StateTimechk[2] = Time.time;
+        }
+        else if (Time.time - StateTimechk[2] >= 4f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
